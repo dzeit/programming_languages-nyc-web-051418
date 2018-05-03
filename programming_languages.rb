2d1ed88d27d7aceec4cc1123ef916f6d08766f37
David Zeit <dzeit216@gmail.com>
2018-05-03 15:30:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,10 +4,12 @@ def reformat_languages(languages)
     languages.each do |style, lang|
       lang.each do |lang_name, type|
         new_hash[lang_name] = type
-        binding.pry if lang_name == :javascript 
+         
         if !new_hash[lang_name][:style]
+          
         new_hash[lang_name][:style] = []
       end 
+      
       new_hash[lang_name][:style].push(style) 
     end 
   end
